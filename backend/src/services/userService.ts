0500eadc6cf7dfb300bfbb4e4a398e8fd4df6a38
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2026-06-26 09:17:03 +0000

    creando endpoint de user

--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,18 +1,73 @@
+// Esta página de servicios es donde se configura la comunicación a la base de datos
+// es el segundo paso que hay que hacer despues de haber configurado el prisma client y
+// el schema.prisma
+// aqui trabajamos con PRISMA como hacer las consultas, (lee la documentación) estas funciones luego seran utilizadas para 
+// solaparlas con las peticiones de express (los HTTP a través del front).
+
 import prisma from "../prisma/client"
 import bcrypt from "bcryptjs";
 
-class UserService{
 
-    async createUser({email, name, passwordHash}) {
-        const existingUser = await prisma.user.findUnique({
-            where: {email},
-        });
+export const createUser = async ({ email, name, passwordHash }) => {
+    const existingUser = await prisma.user.findUnique({
+        where: { email },
+    });
 
-        if(existingUser){
-            throw new Error("Usuario ya registrado.")
+    if (existingUser) {
+        throw new Error("Usuario ya registrado.")
+    }
+
+    const hashedPassword = await bcrypt.hash(passwordHash, 10)
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email,
+            passwordHash: hashedPassword,
         }
+    })
+}
 
-        const hashedPassword = await bcrypt.hash(passwordHash, 10)
+
+export const getUsers = async () => {
+    const allsUser = prisma.user.findMany({
+        select: {
+            id: true,
+            name: true,
+            lastname: true,
+            email: true,
+            role: true
+        }
+    });
+
+    return allsUser;
+}
+
+export const getUser = async (id) => {
+    const userId = prisma.user.findUnique({
+        where: { id }
+    })
+
+    if (!userId) {
+        throw new Error("Usuario no encontrado")
+    } else {
+        return userId;
     }
 }
-// no he terminado este servicio
+
+export const updateUser = async ({ name, email, lastname, passwordHash, profile_image, creditcard }) => {
+    const update = prisma.user.update({
+        where: {
+            id
+        },
+        data: {
+            name,
+            email,
+            lastname,
+            passwordHash,
+            profile_image,
+            creditcard
+
+        }
+    })
+    return update;
+} 
